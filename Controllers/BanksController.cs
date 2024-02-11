4ba5c4d06b6e9650317a30ae8f704f16d5d5e492
KolyaShabalinskiy <kolya.shabalinskiy@gmail.com>
2024-02-11 23:02:08 +0300

    BanksAPI

---
+++ Controllers/BanksController.cs
@@ -0,0 +1,31 @@
+﻿using HackathonTask.Models.MyApp;
+using HackathonTask.Services;
+using HackathonTask.Services.BankInfo;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HackathonTask.Controllers
+{
+    [Route("api/banks")]
+    [ApiController]
+    public class BanksController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> AvailiableBanks() 
+        {
+            return new BanksService().GetAvailiableBanksNames();        
+        }
+        
+        [HttpGet("{bankName}/currencies")]
+        public Task<IEnumerable<string>> GetAvailableCurrencies(string bankName)
+        {
+            return new BanksService().GetCurrencies(bankName);            
+        }
+
+        [HttpGet("rate/{bankName}/{cur}/{date}")]
+        public Task<RateModel> GetRateByDate(string bankName, string cur, DateTime date )
+        {
+            return new BanksService().GetByDate(bankName, cur, date);            
+        }
+    }
+}
---
+++ Services/BanksService.cs
@@ -0,0 +1,38 @@
+﻿using HackathonTask.Models.MyApp;
+using HackathonTask.Services.BankInfo;
+
+namespace HackathonTask.Services
+{
+    public class BanksService
+    {
+        public List<IBankInfo> Banks { get; }
+       
+
+        public BanksService() 
+        { 
+            Banks = new List<IBankInfo>() { new AlfaBankInfo(), new BelarusBankInfo() };        
+        }
+
+        public IEnumerable<string> GetAvailiableBanksNames()
+        {
+            string[] result = new string[Banks.Count];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = Banks[i].BankName;
+            }
+            return result;
+        }
+
+        public async Task<IEnumerable<string>> GetCurrencies(string bankName)
+        {            
+            var bank = Banks.Where(x => x.BankName == bankName).FirstOrDefault();
+            return await bank.GetAvailableCurrencies();
+        }
+
+        public async Task<RateModel> GetByDate(string bankName, string cur, DateTime date)
+        {
+            var bank = Banks.Where(x => x.BankName == bankName).FirstOrDefault();
+            return await bank.GetRateByDate(cur, date);
+        }
+    }
+}
